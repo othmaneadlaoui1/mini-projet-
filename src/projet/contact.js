89afe6da0936549fc4d3e9404911c8dca0e6e718
othmane adlaoui <othmane63392@gmail.com>
2025-01-28 17:57:36 +0100

    test

--- src/projet/contact.js
+++ src/projet/contact.js
@@ -5,14 +5,14 @@ export default function Contact() {
     return (
         <div className="container">
             <div className="info">
-                <h1>Information pour Othmane Adlaoui</h1>
+                <h1> Othmane Adlaoui</h1>
                 <hr />
                 <p>Email : <a href="mailto:othmaneadlaoui@gmail.com">othmaneadlaoui1212@gmail.com</a></p>
                 <p>Num : 0644291173</p>
                 <p>Adresse : Souk Sebt oulad nemma</p>
             </div>
             <div className="info">
-                <h1>Information pour Hamza Tijani</h1>
+                <h1> Hamza Tijani</h1>
                 <hr />
                 <p>Email : <a href="mailto:othmaneadlaoui@gmail.com">?????@gmail.com</a></p>
                 <p>Num : 06000600060</p>
